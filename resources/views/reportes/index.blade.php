24ebb0d36106a0634d7afacf4586c914bbb17899
ilustroc <62394589+ilustroc@users.noreply.github.com>
2025-11-04 10:25:09 -0500

    En la vista index de reportes, estoy agregando los botones de acceso a los diferentes reprotes y esto es una prueba

--- resources/views/reportes/index.blade.php
+++ resources/views/reportes/index.blade.php
@@ -3,4 +3,11 @@
 @section('content')
 <h2>Generar Reportes</h2>
 <p>Aquí irá el módulo para generar y descargar reportes.</p>
+
+<p>
+  <a class="btn" href="{{ route('reportes.impulse.index') }}">Reporte Impulse</a>
+  <a class="btn" href="{{ route('reportes.kp.index') }}">Reporte Kp Invest</a>
+  <a class="btn" href="{{ route('reportes.tec.index') }}">Reporte Tec Center</a>
+  <a class="btn" href="{{ route('reportes.carteras.index') }}">Reporte Carteras</a>
+</p>
 @endsection
